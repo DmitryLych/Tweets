f50231eee9cc95decef399f8bc4f4769b9da5642
Dmitry Lych <dmitrylych@gmail.com>
2017-05-03 12:26:15 +0300

    Threads

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,15 @@
 package com.company;
 
 import com.company.inputOutput.InputFile;
+import com.company.threads.FirstThread;
+import com.company.threads.SecondThread;
+import com.company.threads.ThirdThread;
 import org.json.simple.parser.ParseException;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class Main{
     public static void main(String[] args)  {
      int startLineTweets=Integer.parseInt(args[0]);
      int endLineTweets=Integer.parseInt(args[1]);
@@ -21,16 +24,13 @@ public class Main {
      if(numberOfReport==1) firstReport=true;
      else if(numberOfReport==2)secondReport=true;
      else if(numberOfReport==3)thirdReport=true;
-     InputFile in=new InputFile();
-        try {
-        in.input(startLineTweets,endLineTweets,"E:\\trends\\trends\\trends\\data\\all_tweets.txt",firstReport);
-        in.input(startLineSent,endLineSent,"E:\\trends\\trends\\trends\\data\\sentiments.csv",secondReport);
-        in.input(startLineState,endLineState,"E:\\trends\\trends\\trends\\data\\states.json",thirdReport);
-        } catch (ParseException e) {
-        e.printStackTrace();
-        } catch (java.text.ParseException e) {
-            e.printStackTrace();
-        }
+        FirstThread firstThread=new FirstThread(startLineTweets,endLineTweets,firstReport);
+        firstThread.start();
+        SecondThread secondThread=new SecondThread(startLineSent,endLineSent,secondReport);
+        secondThread.start();
+        ThirdThread thirdThread=new ThirdThread(startLineState,endLineState,thirdReport);
+        thirdThread.start();
+
 
     }
 }
